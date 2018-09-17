0892efa3ed96230eca8391f37263f0acfd5bc101
mau5atron <gabrielbetancourt96@outlook.com>
2018-09-17 03:34:39 -0400

    edited routing for user confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+	as :user do 
+		put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+	end
+
+  devise_for :users, controllers: {
+  	registrations: 'registrations',
+  	confirmation: 'confirmations',
+  }
   root 'home#index'
 end
